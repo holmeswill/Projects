bb25e021e5bf1580b16bdae1a00e5df60c6b1050
Will <williamholmeshewett1@gmail.com>
2017-02-09 21:19:16 +1300

    Added 10 wickets and a chance to get out Updated text on scoring runs

--- Cricket.py
+++ Cricket.py
@@ -5,13 +5,21 @@ balls=0
 overs=10
 runs=0
 i=0
+wickets=10
 
 
 for i in range(0,6):
    hit = random.randint(0,6)
    runs = runs+hit 
-   print ("you got a " +str(hit)+" you now have " + str(runs))
+   print ("you got a " +str(hit)+" you now have " + str(runs)+ "runs")
    
+   out=random.randint(0,100)
+   if out >= 50:
+       wickets=wickets-1
+       print ("you're out you now have "+str(wickets)+ " wickets left")
+       
+   
+
    
    
 
